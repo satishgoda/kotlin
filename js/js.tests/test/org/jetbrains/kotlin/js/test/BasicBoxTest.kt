9efe6aa4b08d49a8db14b266540b57bc696739da
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-05-23 19:50:38 +0300

    Use source map remapper in JS inliner

--- js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
@@ -83,9 +83,7 @@ abstract class BasicBoxTest(
         val outputPostfixFile = getOutputPostfixFile(filePath)
 
         TestFileFactoryImpl().use { testFactory ->
-            testFactory.defaultModule.moduleKind
-
-            val inputFiles = KotlinTestUtils.createTestFiles(file.name, fileContent, testFactory)
+            val inputFiles = KotlinTestUtils.createTestFiles(file.name, fileContent, testFactory, true)
             val modules = inputFiles
                     .map { it.module }.distinct()
                     .map { it.name to it }.toMap()
@@ -534,7 +532,7 @@ abstract class BasicBoxTest(
         private val METADATA_EXTENSION = "jsmeta"
         private val HEADER_FILE = "header.$METADATA_EXTENSION"
 
-        private val TEST_MODULE = "JS_TESTS"
+        val TEST_MODULE = "JS_TESTS"
         private val DEFAULT_MODULE = "main"
         private val TEST_FUNCTION = "box"
         private val OLD_MODULE_SUFFIX = "-old"
