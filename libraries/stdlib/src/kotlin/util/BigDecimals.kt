f3ce94849646be2c516b7a742cf473c357384e57
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-05-24 07:57:41 +0300

    Code formatting

--- libraries/stdlib/src/kotlin/util/BigDecimals.kt
+++ libraries/stdlib/src/kotlin/util/BigDecimals.kt
@@ -1,6 +1,7 @@
 @file:kotlin.jvm.JvmMultifileClass
 @file:kotlin.jvm.JvmName("MathKt")
 @file:kotlin.jvm.JvmVersion
+
 package kotlin
 
 import java.math.BigDecimal
@@ -10,19 +11,19 @@ import java.math.RoundingMode
  * Enables the use of the `+` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.plus(other: BigDecimal) : BigDecimal = this.add(other)
+public inline operator fun BigDecimal.plus(other: BigDecimal): BigDecimal = this.add(other)
 
 /**
  * Enables the use of the `-` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.minus(other: BigDecimal) : BigDecimal = this.subtract(other)
+public inline operator fun BigDecimal.minus(other: BigDecimal): BigDecimal = this.subtract(other)
 
 /**
  * Enables the use of the `*` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.times(other: BigDecimal) : BigDecimal = this.multiply(other)
+public inline operator fun BigDecimal.times(other: BigDecimal): BigDecimal = this.multiply(other)
 
 /**
  * Enables the use of the `/` operator for [BigDecimal] instances.
@@ -31,53 +32,53 @@ public inline operator fun BigDecimal.times(other: BigDecimal) : BigDecimal = th
  * rounding mode is used.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.div(other: BigDecimal) : BigDecimal = this.divide(other, RoundingMode.HALF_EVEN)
+public inline operator fun BigDecimal.div(other: BigDecimal): BigDecimal = this.divide(other, RoundingMode.HALF_EVEN)
 
 /**
  * Enables the use of the `%` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
 @Deprecated("Use rem(other) instead", ReplaceWith("rem(other)"), DeprecationLevel.WARNING)
-public inline operator fun BigDecimal.mod(other: BigDecimal) : BigDecimal = this.remainder(other)
+public inline operator fun BigDecimal.mod(other: BigDecimal): BigDecimal = this.remainder(other)
 
 /**
  * Enables the use of the `%` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.rem(other: BigDecimal) : BigDecimal = this.remainder(other)
+public inline operator fun BigDecimal.rem(other: BigDecimal): BigDecimal = this.remainder(other)
 
 /**
  * Enables the use of the unary `-` operator for [BigDecimal] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.unaryMinus() : BigDecimal = this.negate()
+public inline operator fun BigDecimal.unaryMinus(): BigDecimal = this.negate()
 
 /**
  * Enables the use of the unary `++` operator for [BigDecimal] instances.
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.inc() : BigDecimal = this.add(BigDecimal.ONE)
+public inline operator fun BigDecimal.inc(): BigDecimal = this.add(BigDecimal.ONE)
 
 /**
  * Enables the use of the unary `--` operator for [BigDecimal] instances.
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline operator fun BigDecimal.dec() : BigDecimal = this.subtract(BigDecimal.ONE)
+public inline operator fun BigDecimal.dec(): BigDecimal = this.subtract(BigDecimal.ONE)
 
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun String.toBigDecimal() : BigDecimal = BigDecimal(this)
+public inline fun String.toBigDecimal(): BigDecimal = BigDecimal(this)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun Int.toBigDecimal() : BigDecimal = BigDecimal(this)
+public inline fun Int.toBigDecimal(): BigDecimal = BigDecimal(this)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun Long.toBigDecimal() : BigDecimal = BigDecimal(this)
+public inline fun Long.toBigDecimal(): BigDecimal = BigDecimal(this)
 
 /*
  * JDK documentation recommends using `BigDecimal(String)` instead of `BigDecimal(double)`
@@ -85,7 +86,7 @@ public inline fun Long.toBigDecimal() : BigDecimal = BigDecimal(this)
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun Float.toBigDecimal() : BigDecimal = BigDecimal(this.toString())
+public inline fun Float.toBigDecimal(): BigDecimal = BigDecimal(this.toString())
 
 /*
  * JDK documentation recommends using `BigDecimal(String)` instead of `BigDecimal(double)`
@@ -93,4 +94,4 @@ public inline fun Float.toBigDecimal() : BigDecimal = BigDecimal(this.toString()
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun Double.toBigDecimal() : BigDecimal = BigDecimal(this.toString())
+public inline fun Double.toBigDecimal(): BigDecimal = BigDecimal(this.toString())
--- libraries/stdlib/src/kotlin/util/BigIntegers.kt
+++ libraries/stdlib/src/kotlin/util/BigIntegers.kt
@@ -1,6 +1,7 @@
 @file:kotlin.jvm.JvmMultifileClass
 @file:kotlin.jvm.JvmName("MathKt")
 @file:kotlin.jvm.JvmVersion
+
 package kotlin
 
 import java.math.BigInteger
@@ -10,92 +11,92 @@ import java.math.BigDecimal
  * Enables the use of the `+` operator for [BigInteger] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.plus(other: BigInteger) : BigInteger = this.add(other)
+public inline operator fun BigInteger.plus(other: BigInteger): BigInteger = this.add(other)
 
 /**
  * Enables the use of the `-` operator for [BigInteger] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.minus(other: BigInteger) : BigInteger = this.subtract(other)
+public inline operator fun BigInteger.minus(other: BigInteger): BigInteger = this.subtract(other)
 
 /**
  * Enables the use of the `*` operator for [BigInteger] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.times(other: BigInteger) : BigInteger = this.multiply(other)
+public inline operator fun BigInteger.times(other: BigInteger): BigInteger = this.multiply(other)
 
 /**
  * Enables the use of the `/` operator for [BigInteger] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.div(other: BigInteger) : BigInteger = this.divide(other)
+public inline operator fun BigInteger.div(other: BigInteger): BigInteger = this.divide(other)
 
 /**
  * Enables the use of the `%` operator for [BigInteger] instances.
  */
 @SinceKotlin("1.1")
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.rem(other: BigInteger) : BigInteger = this.remainder(other)
+public inline operator fun BigInteger.rem(other: BigInteger): BigInteger = this.remainder(other)
 
 /**
  * Enables the use of the unary `-` operator for [BigInteger] instances.
  */
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.unaryMinus() : BigInteger = this.negate()
+public inline operator fun BigInteger.unaryMinus(): BigInteger = this.negate()
 
 /**
  * Enables the use of the `++` operator for [BigInteger] instances.
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.inc() : BigInteger = this.add(BigInteger.ONE)
+public inline operator fun BigInteger.inc(): BigInteger = this.add(BigInteger.ONE)
 
 /**
  * Enables the use of the `--` operator for [BigInteger] instances.
  */
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline operator fun BigInteger.dec() : BigInteger = this.subtract(BigInteger.ONE)
+public inline operator fun BigInteger.dec(): BigInteger = this.subtract(BigInteger.ONE)
 
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun BigInteger.inv() : BigInteger = this.not()
+public inline fun BigInteger.inv(): BigInteger = this.not()
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline infix fun BigInteger.and(other: BigInteger) : BigInteger = this.and(other)
+public inline infix fun BigInteger.and(other: BigInteger): BigInteger = this.and(other)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline infix fun BigInteger.or(other: BigInteger) : BigInteger = this.or(other)
+public inline infix fun BigInteger.or(other: BigInteger): BigInteger = this.or(other)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline infix fun BigInteger.xor(other: BigInteger) : BigInteger = this.xor(other)
+public inline infix fun BigInteger.xor(other: BigInteger): BigInteger = this.xor(other)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline infix fun BigInteger.shl(n: Int) : BigInteger = this.shiftLeft(n)
+public inline infix fun BigInteger.shl(n: Int): BigInteger = this.shiftLeft(n)
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline infix fun BigInteger.shr(n: Int) : BigInteger = this.shiftRight(n)
-
-
-@SinceKotlin("1.2")
-@kotlin.internal.InlineOnly
-public inline fun String.toBigInteger() : BigInteger = BigInteger(this)
-
-@SinceKotlin("1.2")
-@kotlin.internal.InlineOnly
-public inline fun Int.toBigInteger() : BigInteger = BigInteger.valueOf(this.toLong())
-
-@SinceKotlin("1.2")
-@kotlin.internal.InlineOnly
-public inline fun Long.toBigInteger() : BigInteger = BigInteger.valueOf(this)
+public inline infix fun BigInteger.shr(n: Int): BigInteger = this.shiftRight(n)
 
 
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
-public inline fun BigInteger.toBigDecimal() : BigDecimal = BigDecimal(this)
+public inline fun String.toBigInteger(): BigInteger = BigInteger(this)
+
+@SinceKotlin("1.2")
+@kotlin.internal.InlineOnly
+public inline fun Int.toBigInteger(): BigInteger = BigInteger.valueOf(this.toLong())
+
+@SinceKotlin("1.2")
+@kotlin.internal.InlineOnly
+public inline fun Long.toBigInteger(): BigInteger = BigInteger.valueOf(this)
+
+
+@SinceKotlin("1.2")
+@kotlin.internal.InlineOnly
+public inline fun BigInteger.toBigDecimal(): BigDecimal = BigDecimal(this)
